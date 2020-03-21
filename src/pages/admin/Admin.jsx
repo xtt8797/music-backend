891099ad8cbdc3b032154f366f2929908f3c1535
xtt <xtt8797@gmail.com>
2020-03-21 15:36:24 -0600

    '3.21'

---
+++ src/pages/admin/Admin.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Button} from 'antd';
+
+class Admin extends Component {
+    render() {
+        return (
+            <div>
+                <Button type='primary'> nihao</Button>
+            </div>
+        );
+    }
+}
+
+export default Admin;
